3632990e4372f44000f70684172b2f6d8b6e158f
Tuyen <nhoktuyenpro@gmail.com>
2020-06-23 17:38:22 +0700

    [Backend] initial data sql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO role (name) VALUES ('Team Lead'), ('Product Owner'), ('Team Member'), ('Architecture Owner'), ('Stakeholder');
+
+INSERT INTO priority (name, `_order`) VALUES ('Low', 1), ('Medium', 2), ('High', 3);
