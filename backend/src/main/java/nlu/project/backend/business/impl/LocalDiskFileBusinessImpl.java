cab2160ecfa457881261780ca002c0e4c061c023
16130586 <16130586@st.hcmuaf.edu.vn>
2020-07-06 14:25:39 +0700

    [backend] update

--- backend/src/main/java/nlu/project/backend/business/impl/LocalDiskFileBusinessImpl.java
+++ backend/src/main/java/nlu/project/backend/business/impl/LocalDiskFileBusinessImpl.java
@@ -13,13 +13,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
-public class FileBusinessImpl implements FileBusiness {
+public class LocalDiskFileBusinessImpl implements FileBusiness {
     @Value("${upload.project.image.absolute.path}")
-    private String ABSOLUTE_PATH_IMAGE_STORAGE = (new File("src/main/upload")).getAbsolutePath();
-
+    private String ABSOLUTE_PATH_IMAGE_STORAGE;
+    private final String DEFAULT_PATH_IMAGE_STORAGE = "src/main/upload";
+    private final String ACTION = "/storage/";
 
     @Override
     public String save(MultipartFile file) {
+        if(ABSOLUTE_PATH_IMAGE_STORAGE == null || ABSOLUTE_PATH_IMAGE_STORAGE.equals("") || ABSOLUTE_PATH_IMAGE_STORAGE.length() == 0){
+            ABSOLUTE_PATH_IMAGE_STORAGE = (new File(DEFAULT_PATH_IMAGE_STORAGE)).getAbsolutePath();
+        }
+
         if (file.isEmpty())
             return "";
         File directory = new File(ABSOLUTE_PATH_IMAGE_STORAGE);
@@ -29,14 +34,16 @@ public class FileBusinessImpl implements FileBusiness {
         String fileName = StringUtils.randomString() + "_" + System.currentTimeMillis() + ".jpg";
         String path = ABSOLUTE_PATH_IMAGE_STORAGE + File.separator + fileName;
         File serverFile = new File(path);
+
         try {
             BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
             stream.write(file.getBytes());
+            stream.flush();
             stream.close();
         } catch (IOException ex) {
             throw new InternalException("Upload file error!");
         }
-        return fileName;
+        return ACTION.concat(fileName);
     }
 
     @Override
